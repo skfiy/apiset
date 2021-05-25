25f8c3860d70dbd82f525092fea94611be323ba2
unknown <1923021@qq.com>
2021-05-25 15:29:07 +0800

    增加个农历查询接口

--- main.go
+++ main.go
@@ -14,20 +14,19 @@ import (
 	"time"
 )
 
-const GO_URL = "http://localhost:8081";
+const GO_URL = "http://localhost:8081"
 
 type MsgInfo struct {
-	 Code int   `json:"code"`
-	 Msg  string   `json:"msg"`
-	 Data interface{}  `json:"data"`
-	 Time string `json:"time"`
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+	Time string      `json:"time"`
 }
 
-
-func  MsgData(code int,msg string,data interface{}) string  {
-	  msgs:=  &MsgInfo{code,msg,data,time.Now().Format("2006-01-02 15:04:05")}
-	  Info,_ := json.Marshal(msgs)
-	  return string(Info)
+func MsgData(code int, msg string, data interface{}) string {
+	msgs := &MsgInfo{code, msg, data, time.Now().Format("2006-01-02 15:04:05")}
+	Info, _ := json.Marshal(msgs)
+	return string(Info)
 }
 
 /**
@@ -50,74 +49,76 @@ func GetDestinyDataSet(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func TextConversionMp3(w http.ResponseWriter, r *http.Request){
+func TextConversionMp3(w http.ResponseWriter, r *http.Request) {
 
 	err := r.ParseForm()
 	if err != nil {
 		log.Fatal("系统错误" + err.Error())
 	}
-	var  msg string
+	var msg string
 	content := r.Form.Get("content")
-		s,_:= ioutil.ReadAll(r.Body)
-		fmt.Println(s)
-	if content == ""  {
-		msg = MsgData(200,"参数值为空","");
-		fmt.Fprintf(w, "%s", msg);
-	}else{
+	s, _ := ioutil.ReadAll(r.Body)
+	fmt.Println(s)
+	if content == "" {
+		msg = MsgData(200, "参数值为空", "")
+		fmt.Fprintf(w, "%s", msg)
+	} else {
 		msg = tm.TextMp3(content)
-		fmt.Fprintf(w, "%s", msg);
+		fmt.Fprintf(w, "%s", msg)
 	}
-	fmt.Println(content);
+	fmt.Println(content)
 
 }
 
-func Mp3Init(w http.ResponseWriter, r *http.Request){
+func Mp3Init(w http.ResponseWriter, r *http.Request) {
 	realPath := r.URL.String()
-	fmt.Println(realPath);
-	file,err :=os.Open("."+realPath)
+	fmt.Println(realPath)
+	file, err := os.Open("." + realPath)
 	defer file.Close()
 	if err != nil {
 		log.Println("static resource:", err)
 		w.WriteHeader(404)
 	} else {
-		bs,_ := ioutil.ReadAll(file)
+		bs, _ := ioutil.ReadAll(file)
 
 		w.Write(bs)
 	}
 }
 
 //首页
-func Index(w http.ResponseWriter, r *http.Request){
-	q := "如有问题请联系QQ:1923021";
-	s := "------API------";
-	z := "\r  /text?content=      //文字转语音获取MP3\n" +
-		 "\r  /destiny            //命运2周报\n" +
-		 "\r  /getip            //获取IP地址位置\n" +
-		 "\r ------API------";
+func Index(w http.ResponseWriter, r *http.Request) {
+	q := "如有问题请联系QQ:1923021"
+	s := "------API------"
+	z := "\r  地址 api.cloolc.club 以下接口\n" +
+		"\r  /text?content=       //文字转语音获取MP3\n" +
+		"\r  /destiny             //命运2周报\n" +
+		"\r  /getip               //获取IP地址位置\n" +
+		"\r  /lunar               //获取农历日\n" +
+		"\r ------API------"
 
-	fmt.Fprintf(w, "  %s \n %s\n%s\n", q,s,z);
+	fmt.Fprintf(w, "  %s \n %s\n%s\n", q, s, z)
 
 }
 
-func GetIp(w http.ResponseWriter, r *http.Request){
+func GetIp(w http.ResponseWriter, r *http.Request) {
 
-	w.Header().Set("Access-Control-Allow-Origin", "*") //允许访问所有域
+	w.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
 	w.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
-	w.Header().Set("content-type", "application/json") //返回数据格式是json
+	w.Header().Set("content-type", "application/json")             //返回数据格式是json
 	//获取IP地址
-	ip := ClientPublicIP(r);
-	if ip =="" {
-		ip = ClientIP(r);
+	ip := ClientPublicIP(r)
+	if ip == "" {
+		ip = ClientIP(r)
 	}
-	json :=  Ipcity(ip);
+	json := Ipcity(ip)
 
-	fmt.Fprintf(w, "%s",json);
+	fmt.Fprintf(w, "%s", json)
 
 }
 
 //查询 IP 位置
-func Ipcity(ip string) string{
-	url := "http://ip.taobao.com/outGetIpInfo?ip="+ip+"&accessKey=alibaba-inc";
+func Ipcity(ip string) string {
+	url := "http://ip.taobao.com/outGetIpInfo?ip=" + ip + "&accessKey=alibaba-inc"
 	resp, err := http.Get(url)
 	defer resp.Body.Close()
 	if err != nil {
@@ -127,7 +128,22 @@ func Ipcity(ip string) string{
 	if err != nil {
 		fmt.Println(err)
 	}
-	return string(data);
+	return string(data)
+}
+
+//查询农历日
+func Lunar(w http.ResponseWriter, r *http.Request) {
+	url := "http://www.skfiy.com/lunar/index.php"
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Fprintf(w, "%s", data)
 }
 
 //获取当前请求用户IP地址
@@ -156,25 +172,25 @@ func ClientPublicIP(r *http.Request) string {
 	for _, ip = range strings.Split(r.Header.Get("X-Forwarded-For"), ",") {
 		ip = strings.TrimSpace(ip)
 		fmt.Println(ip)
-		if ip != ""  {
+		if ip != "" {
 			return ip
 		}
 	}
 
 	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
-	if ip != ""  {
+	if ip != "" {
 		return ip
 	}
 	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
-			return ip
+		return ip
 	}
 	return ""
 }
 
-
 func main() {
-	http.HandleFunc("/getip",GetIp)
-	http.HandleFunc("/mp3/",Mp3Init)
+	http.HandleFunc("/lunar", Lunar)
+	http.HandleFunc("/getip", GetIp)
+	http.HandleFunc("/mp3/", Mp3Init)
 	http.HandleFunc("/text", TextConversionMp3)
 	http.HandleFunc("/destiny", GetDestinyDataSet)
 	http.HandleFunc("/", Index)
@@ -196,7 +212,7 @@ func main() {
 			break
 		}
 		log.Println("SERVER 启动成功!")
-		log.Println("URL：",GO_URL)
+		log.Println("URL：", GO_URL)
 	}()
 	err := http.ListenAndServe(":8081", nil) // 设置监听的端口
 	if err != nil {
